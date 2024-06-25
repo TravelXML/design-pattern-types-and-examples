fb977d460878e6763691364ceff98260697cee21
Sapan Kumar Mohanty <apigility@gmail.com>
2024-06-25 10:55:09 +0530

    Create Product.php

---
+++ Factory Pattern/Product.php
@@ -0,0 +1,13 @@
+<?php
+class ConcreteProductA implements Product {
+    public function getType() {
+        return "Type A";
+    }
+}
+
+class ConcreteProductB implements Product {
+    public function getType() {
+        return "Type B";
+    }
+}
+?>
